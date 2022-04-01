60169da287120e6fec7f10d4a948e1aabb7b74d2
unknown <adrian.kanox.2003@gmail.com>
2022-04-01 11:18:45 -0500

    home page

--- config/db.js
+++ config/db.js
@@ -4,6 +4,9 @@ const users = [
         name: 'admin',
         password: 'admin',
         shortname: 'Admin',
+        allname: 'Administrador',
+        opegin_id: '0000',
+        cc: '0000',
         role: 'admin'
     },
     {
@@ -11,21 +14,30 @@ const users = [
         name: 'jose',
         password: 'jose',
         shortname: 'José C.',
-        role: 'gang-leader'
+        allname: 'José Camargo',
+        opegin_id: '069-TD',
+        cc: '1118525948',
+        role: 'Técnico'
     },
     {
         id: 3,
         name: 'manolo',
         password: 'manolo',
         shortname: 'Manolo G.',
-        role: 'supervisor'
+        allname: 'Manolo García',
+        opegin_id: '071-TD',
+        cc: '6749473',
+        role: 'Supervisor'
     },
     {
         id: 4,
         name: 'caraquea',
         password: 'opegin',
         shortname: 'Carlos A.',
-        role: 'gang-leader'
+        allname: 'Carlos Elí Araque',
+        opegin_id: '079-TD',
+        cc: '184638292',
+        role: 'Técnico'
     },
 ];
 
